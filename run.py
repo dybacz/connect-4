3cec110a96a9c73dab2176e43b9a5d92a2a17450
dybacz <jdybacz@gmail.com>
2021-10-10 13:08:19 +0000

    Initial Commit, new_game function added

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def new_game():
+
+    #num_players = input("Enter 1 for Singleplayer or 2 for Multiplayer: \n")
+    num_players = 2
+    print("*" + ("-" * 36) + "*")
+    print("*" + (" " * 4) + "Welcome to Python CONNECT-4!" + (" " * 4) + "*")
+    print("*" + (" " * 4) + "Game type: Classic" + (" " * 14) + "*")
+    print("*" + (" " * 4) + "Board size: Standard" + (" " * 12) + "*")
+    print("*" + (" " * 4) + "Vs: Computer" + (" " * 20) + "*")
+    print("*" + ("-" * 36) + "*")
+
+    player_names = []
+    if num_players == 1:
+        player_names = input("Please enter your name: \n")
+    elif num_players == 2:
+        player_name = input("Player One - Please enter your name: \n")
+        player_names.append(player_name)
+        player_name = input("Player Two - Please enter your name: \n")
+        player_names.append(player_name)
+
+    print(player_names)
+
+
+new_game()
