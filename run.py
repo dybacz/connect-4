6bd9e8fe481c88d4552bb7b1870b9f4fc136c017
dybacz <jdybacz@gmail.com>
2021-10-10 13:39:15 +0000

    Player class added, create instance of player with name, colour, type

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
-def new_game():
+class Player:
+    """
+    Player class, Creates player, sets player name, counter colour, player type
+    (Real or Computer)
+    """
 
+    def __init__(self, name, counter_color, type):
+        self.name = name
+        self.color = counter_color
+        self.type = type
+        print(self.name, self.color, self.type)
+
+
+def new_game():
+    """
+    Starts a new game, collects player name.
+    """
     #num_players = input("Enter 1 for Singleplayer or 2 for Multiplayer: \n")
     num_players = 2
     print("*" + ("-" * 36) + "*")
@@ -11,14 +26,17 @@ def new_game():
 
     player_names = []
     if num_players == 1:
-        player_names = input("Please enter your name: \n")
+        player_name = input("Please enter your name: \n")
+        player_names.append(player_name)
+        player_name = "Computer"
+        player_names.append(player_name)
     elif num_players == 2:
         player_name = input("Player One - Please enter your name: \n")
         player_names.append(player_name)
         player_name = input("Player Two - Please enter your name: \n")
         player_names.append(player_name)
 
-    print(player_names)
-
+    player_one = Player(player_names[0], "blue", "player")
+    player_two = Player(player_names[1], "red", "player")
 
 new_game()
